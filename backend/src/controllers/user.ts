f7113e3438240d41982c84d4d69296eeaf388dfb
Ohad Shalom <ohadshalom@Ohads-MacBook-Pro.local>
2022-07-20 17:06:00 +0300

    user edit added to path

--- backend/src/controllers/user.ts
+++ backend/src/controllers/user.ts
@@ -1,6 +1,9 @@
-import User from '../models/user_model'
+import UserModel from '../models/user_model'
 import { Request,Response } from 'express'
 import { StatusCodes } from "http-status-codes";
+import bcrypt from 'bcrypt'
+import { use } from '../routes/post_routes';
+
 
 /**
  * findUserByEmail
@@ -16,7 +19,7 @@ export const findUserByEmail =async (req: Request, res: Response) => {
   }
 
   try {
-    const user = await User.findById(email);
+    const user = await UserModel.findById(email);
     if (user == null) {
       res.status(400).send({
         err: "user doesnot exists",
@@ -30,6 +33,52 @@ export const findUserByEmail =async (req: Request, res: Response) => {
     });
   }
 }
+
+
+
+/**
+ * EditUser
+ * @param req html request
+ * @param res html respond
+ * @returns User
+ */
+ export const editUser =async (req: Request, res: Response) => {
+  const oldEmail = req.params.oldEmail;
+  const newEmail = req.params.newEmail;
+  const password = req.params.passowrd
+  const imageUri = req.params.imageUri
+
+  console.log("edditing user " + req.params.id);
+if (oldEmail == null || oldEmail == undefined) {
+  return res.status(400).send({ err: "no email provided" });
+}
+
+try {
+  const user = await UserModel.findById(oldEmail);
+  if (user == null) {
+    res.status(400).send({
+      err: "user doesnot exists",
+    });
+  } else {
+    user.email=newEmail
+    //encrypt password
+    const salt = await bcrypt.genSalt(10);
+    const encryptedPassword = await bcrypt.hash(password, salt);
+    user.passowrd=encryptedPassword
+    user.imageUri=imageUri
+    const newUser = await user.save() 
+    res.status(200).send({
+      _id:newUser._id
+    });
+  }
+} catch (err) {
+  res.status(400).send({
+    err: err.message,
+  });
+}
+}
+
+
 
 
 
--- backend/src/routes/user_routes.ts
+++ backend/src/routes/user_routes.ts
@@ -1,9 +1,10 @@
 import express from 'express'
 const router = express.Router()
-import {findUserByEmail} from "../controllers/user"
+import {findUserByEmail , editUser} from "../controllers/user"
 import authenticate from '../common/auth_middleware'
 
 router.get('/:email',findUserByEmail)
+router.post('/edit',editUser)
 
 
 export = router
